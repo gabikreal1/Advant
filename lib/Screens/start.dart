a3e4ec7ab16ba9c7c9ed62b457e09a0fa29a7ab5
gabikreal1 <growgabhd@gmail.com>
2023-04-24 23:37:08 +0100

    start_responsive

--- lib/Screens/start.dart
+++ lib/Screens/start.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login/Screens/login.dart';
 import 'package:login/Components/my_button.dart';
+import 'package:login/Screens/web/web_start.dart';
 
 class Start extends StatefulWidget {
   const Start({super.key});
@@ -13,57 +14,63 @@ class Start extends StatefulWidget {
 }
 
 class _StartState extends State<Start> {
-  final _signUpPressed = false;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Column(
-          children: [
-            Flexible(
-                flex: 2,
-                fit: FlexFit.tight,
-                child: Image.asset('assets/background.png')),
-            Flexible(
-              flex: 2,
-              fit: FlexFit.tight,
-              child: Container(),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Column(
-                  children: [
-                    MyButton(
-                      text: "SIGN UP",
-                      onTap: () => Get.to(
-                        () => Login(),
-                        transition: Transition.downToUp,
-                        duration: Duration(milliseconds: 350),
+    return LayoutBuilder(
+      builder: (context, contraints) {
+        if (contraints.maxWidth < 550) {
+          return Scaffold(
+            backgroundColor: Colors.grey[300],
+            body: SafeArea(
+              child: Column(
+                children: [
+                  Flexible(
+                      flex: 2,
+                      fit: FlexFit.tight,
+                      child: Image.asset('assets/background.png')),
+                  Flexible(
+                    flex: 2,
+                    fit: FlexFit.tight,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Column(
+                        children: [
+                          MyButton(
+                            text: "SIGN UP",
+                            onTap: () => Get.to(
+                              () => Login(),
+                              transition: Transition.downToUp,
+                              duration: Duration(milliseconds: 350),
+                            ),
+                            color: Colors.white,
+                            textColor: Colors.black,
+                          ),
+                          SizedBox(height: 15),
+                          MyButton(
+                            color: Colors.black,
+                            text: "SIGN IN",
+                            onTap: () => Get.to(
+                              () => Login(),
+                              transition: Transition.downToUp,
+                              duration: Duration(milliseconds: 350),
+                            ),
+                          ),
+                        ],
                       ),
-                      color: Colors.white,
-                      textColor: Colors.black,
                     ),
-                    SizedBox(height: 15),
-                    MyButton(
-                      color: Colors.black,
-                      text: "SIGN IN",
-                      onTap: () => Get.to(
-                        () => Login(),
-                        transition: Transition.downToUp,
-                        duration: Duration(milliseconds: 350),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return webStart();
+        }
+      },
     );
   }
 }
---
+++ lib/Screens/web/web_start.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login/Components/my_button.dart';
+import 'package:login/Screens/login.dart';
+
+class webStart extends StatefulWidget {
+  const webStart({super.key});
+
+  @override
+  State<webStart> createState() => _webStartState();
+}
+
+class _webStartState extends State<webStart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(flex: 1, child: Container()),
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: SizedBox(
+                  width: 450,
+                  child: Material(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: Colors.grey[300],
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: Image.asset(
+                              "assets/background.png",
+                              width: 250,
+                              height: 250,
+                            ),
+                          ),
+                          MyButton(
+                            onTap: () => Get.to(
+                              () => Login(),
+                              transition: Transition.downToUp,
+                              duration: Duration(milliseconds: 350),
+                            ),
+                            text: "SIGN UP",
+                            color: Colors.white,
+                            textColor: Colors.black,
+                          ),
+                          const SizedBox(height: 10),
+                          MyButton(
+                            onTap: () => {},
+                            text: "SIGN IN",
+                            color: Colors.black,
+                            textColor: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(flex: 2, child: Container())
+          ],
+        ),
+      ),
+    );
+  }
+}
